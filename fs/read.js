174aa023f6b9e25d6a83081b512d5be1d2571ff9
anshusaurav <anshu.saurav@gmail.com>
2020-05-01 17:27:09 +0530

    completed-all

--- fs/read.js
+++ fs/read.js
@@ -33,24 +33,30 @@ var fs = require('fs');
 fs.open('./write.js',(err, fd) => {
     if(err) {
       console.log('Error: ', err);
-    } else {
-        fs.truncate('./write.js',fd.length, (err) => {
-          if(err) {
-            console.log('Error: ', err);
-          } else {
-              fs.writeFile('./write.js', "File written", (err) => {
-                if(err) {
+    } 
+    else {
+      fs.truncate('./write.js',fd.length, (err) => {
+        if(err) {
+          console.log('Error: ', err);
+        } 
+        else {
+          fs.writeFile('./write.js', "File written", (err) => {
+            if(err)
+              console.log('Error: ', err);
+            else {
+              fs.close(fd, (err) => {
+                if(err) 
                   console.log('Error: ', err);
-                } else {
-                    fs.close(fd, (err) => {
-                      if(err) {
-                        console.log('Error: ', err);
-                      }
-                    })
-                }
               })
-          }
-        })
+            }
+          })
+        }
+      })
     }
-  })
+ });
 // 6. Delete the content of write.js using fs.unlink or unlinkSync method
+fs.unlink('./write.js', (err) => {
+  if(err) {
+    console.log('Error: ', err.name);
+  }
+});
